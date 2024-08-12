ab54df0a8c8ca173dbca36d51f06db356c49f1c1
VIJAY KUMAR GOWDA K K <rvit21bis065.rvitm@rvei.edu.in>
2024-08-12 13:06:13 +0530

    Update models.py

--- django1/app3/models.py
+++ django1/app3/models.py
@@ -2,8 +2,8 @@ from django.db import models
 class students(models.Model):
         fname=models.CharField(max_length=50)
         lname=models.CharField(max_length=100)
-        aadhar=models.CharField(max_length=30)
-        phone=models.CharField(max_length=30)
+        aadhar=models.CharField(max_length=50)
+        phone=models.CharField(max_length=50)
         def __str__(self):
                 return self.name1
 

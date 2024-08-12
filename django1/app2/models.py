a69ba394c3ebae8109a56b6937d7360236d78ca8
VIJAY KUMAR GOWDA K K <rvit21bis065.rvitm@rvei.edu.in>
2024-08-12 13:08:54 +0530

    Update models.py

--- django1/app2/models.py
+++ django1/app2/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class students(models.Model):
         name1=models.CharField(max_length=50)
         college1=models.CharField(max_length=100)
-        course1=models.CharField(max_length=30)
+        course1=models.CharField(max_length=50)
         def __str__(self):
                 return self.name1
 
